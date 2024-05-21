2bad99925667c2ff12c55056e4ebcc1b1bca3afa
alexDev2808 <colores.2808.8000@gmail.com>
2024-05-21 13:29:57 -0600

    Mostrar msg al usuario si desea aplicar

--- resources/views/livewire/mostrar-vacante.blade.php
+++ resources/views/livewire/mostrar-vacante.blade.php
@@ -4,7 +4,7 @@
             {{ $vacante->titulo }}
         </h3>
 
-        <div class="md:grid md:grid-cols-2 bg-gray-100 p-4 my-10">
+        <div class="md:grid md:grid-cols-2 bg-gray-50 p-4 my-10">
             <p class="font-bold text-sm text-gray-800 my-3">Empresa: 
                 <span class="normal-case font-normal">{{ $vacante->empresa }}</span>
             </p>
@@ -33,4 +33,13 @@
             <p>{{$vacante->descripcion}}</p>
         </div>
     </div>
+
+    @guest
+        <div class="mt-5 bg-gray-100 border border-dashed p-5 text-center">
+            <p>
+                ¿Deseas aplicar a esta vacante? <a class="font-bold text-indigo-600" href="{{ route('register') }}">Obten una cuenta y aplica a esta y otras vacantes.</a>
+            </p>
+        </div>
+    @endguest
+
 </div>
