8824c2046609dab8212a84e48d268195fd0ec588
alexDev2808 <colores.2808.8000@gmail.com>
2024-05-24 17:56:34 -0600

    Creando el candidato para la vacante

--- app/Http/Livewire/PostularVacante.php
+++ app/Http/Livewire/PostularVacante.php
@@ -2,10 +2,43 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Vacante;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class PostularVacante extends Component
 {
+    public $cv;
+    public $vacante;
+
+    use WithFileUploads;
+
+    protected $rules = [
+        'cv' => 'required|mimes:pdf',
+    ];
+
+    public function mount(Vacante $vacante) {
+        $this->vacante = $vacante;
+    }
+
+    public function postularme() {
+        $datos = $this->validate();
+
+        // Almacenar cv
+        $cv = $this->cv->store('public/cv');
+        $datos['cv'] = str_replace('public/cv/', '', $cv);
+
+        // Crear el candidato para la vacante
+        $this->vacante->candidatos()->create([
+            'user_id' => auth()->user()->id,
+            'cv' => $datos['cv'],
+        ]);
+
+        // Crear notificacion y enviar email
+
+
+        // Mostrar al usuario mensaje de ok
+    }
     public function render()
     {
         return view('livewire.postular-vacante');
